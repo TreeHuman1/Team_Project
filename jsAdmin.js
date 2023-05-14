7eac3b94c456843abd6cd5c04d0ce5e21d30d763
R.cornelius4 <r.cornelius4@my.cs.coloradotech.edu>
2023-05-14 15:40:13 -0600

    commented code

--- jsAdmin.js
+++ jsAdmin.js
@@ -1,10 +1,12 @@
 document.getElementById('discountForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    var pizza = document.getElementById('pizza').value;
-    var discount = document.getElementById('discount').value;
-  
-    // Save the discount in localStorage
-    localStorage.setItem(pizza, discount);
-  });
+  event.preventDefault();
+
+  // Get the values entered in the pizza and discount fields
+  var pizza = document.getElementById('pizza').value;
+  var discount = document.getElementById('discount').value;
+
+  // Save the discount in localStorage
+  localStorage.setItem(pizza, discount);
+});
+
   
